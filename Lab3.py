0e68c7eba41ce0e9c451526d4fd9105886bb7dde
Zeinb Al Nasser <117629744+Zeinb1416@users.noreply.github.com>
2023-03-28 15:37:16 +0300

    Add files via upload

---
+++ Lab3.py
@@ -0,0 +1,11 @@
+#this is the solution for first point
+my_favourite_football_team = "Alhilal"
+#this is the solution for second point
+x,y =17,23.50
+#this is the solution for third point
+Hassan = Abdullah = Ahmad = 86
+#this is the solution for fourth point
+city = "Riyadh", "Jeddah"
+print(my_favourite_football_team)
+print(x,y)
+print(city)
